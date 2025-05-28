8655e744a27b76cf3c7c308c716eabd1080b6da4
Danil <danilbystrov8@gmail.com>
2025-05-28 14:24:52 +0300

    vse sdelano po punktam. vrode rabotaet

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,69 @@
 import { useState } from 'react'
-import reactLogo from '/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-import { MyComponent } from './MyComponent'
-import { CurrentDateComponent } from './currentDate'
+import './App.css'
+import styles from './app.module.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  // const yearNow = new Date().getFullYear();
+	const [value, setValue] = useState('')
+	const [list, setList] = useState([])
+	const [error, setError] = useState('')
+	const [isValueVaild, setIsValueValid] = useState(false)
+
+	function onInputButtonClick() {
+		const promptValue = prompt()
+
+		if (promptValue !== null) {
+			if (promptValue.length >= 3) {
+				setIsValueValid(true)
+				setValue(promptValue)
+				setError('')
+			}
+			else {
+				setIsValueValid(false)
+				setValue('')
+				setError('Введенное значение должно содержать минимум 3 символа')
+			}
+		}
+	}
+
+	function onAddButtonClick() {
+		if (value) {
+			const updatedList = [...list, { id: Date.now(), value: value }];
+            setList(updatedList);
+			setValue('')
+			setError('')
+		}
+	}
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-		{/* <p>Текущий год: {yearNow}</p> */}
-		<MyComponent />
-		{/* <CurrentDateComponent /> */}
-    </>
+    <div className={styles['app']}>
+
+    <h1 className={styles['page-heading']}>Ввод значения</h1>
+
+    <p className={styles['no-margin-text']}>
+      Текущее значение <code>value</code>: "<output className={styles['current-value']}>{value}</output>"
+    </p>
+
+	{error && <div className={styles['error']}>{error}</div>}
+
+    <div className={styles['buttons-container']}>
+      <button className={styles['button']} onClick={onInputButtonClick}>Ввести новое</button>
+      <button className={styles['button']} onClick={onAddButtonClick} disabled={!isValueVaild}>Добавить в список</button>
+    </div>
+
+    <div className={styles['list-container']}>
+      <h2 className={styles['list-heading']}>Список:</h2>
+
+	{list.length > 0 ?
+		<ul className={styles['list']}>
+			{list.map(({id, value}) => <li key={id}>{value}</li>)}
+		</ul>
+		:
+		<p className={styles['no-margin-text']}>Нет добавленных элементов</p>
+	}
+
+    </div>
+  </div>
   )
 }
 
